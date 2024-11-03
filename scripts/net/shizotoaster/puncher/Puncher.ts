aff7495686bd5655a6973154fd7de47a62332b73
shizotoaster <51480483+shizotoaster@users.noreply.github.com>
2024-11-04 05:46:32 +0700

    Puncher 2.0.0

--- scripts/net/shizotoaster/puncher/Puncher.ts
+++ scripts/net/shizotoaster/puncher/Puncher.ts
@@ -1,34 +1,12 @@
-import { world, EntityQueryOptions, Player, Entity } from "@minecraft/server"
+import { world, Player, EntityTypeFamilyComponent, EntityComponent } from "@minecraft/server"
 
-world.afterEvents.entityHitEntity.subscribe((event) => {
-  if (event.damagingEntity.typeId == 'minecraft:player') {
-    let entityQuery: EntityQueryOptions = {
-      families: [ 'boat' ],
-      type: event.hitEntity.typeId,
-      closest: 1,
-      location: event.hitEntity.location
+world.afterEvents.entityHitEntity.subscribe((event) => {  
+  if (event.damagingEntity instanceof Player && event.damagingEntity.isSneaking) {
+    let entityFamilies: EntityComponent | undefined = event.hitEntity.getComponent('minecraft:type_family')
+
+    if (entityFamilies instanceof EntityTypeFamilyComponent && 
+    (entityFamilies.hasTypeFamily('boat') || entityFamilies.hasTypeFamily('minecart'))) {
+      event.hitEntity.applyDamage(Number.MAX_SAFE_INTEGER)
     }
-
-    let testEntity: Entity = event.damagingEntity.dimension.getEntities(entityQuery)[0]
-    
-    let player: Player | undefined
-    world.getPlayers().forEach(plr => { if (plr.id == event.damagingEntity.id) { player = plr } })
-
-    if (player != undefined && testEntity === event.hitEntity && player.isSneaking) { event.hitEntity.applyDamage(100000000000000000) }
-  }
-})
-
-world.afterEvents.entityHitEntity.subscribe((event) => {
-  if (event.damagingEntity.typeId == 'minecraft:player') {
-    let entityQuery: EntityQueryOptions = { families: [ 'minecart' ] }
-
-    let entities: Entity[] = event.damagingEntity.dimension.getEntities(entityQuery)
-    let entity: Entity | undefined
-    entities.forEach(ntt => { if (ntt == event.hitEntity) { entity = ntt } })
-
-    let player: Player | undefined
-    world.getPlayers().forEach(plr => { if (plr.id == event.damagingEntity.id) { player = plr } })
-
-    if (player != undefined && entity != undefined && player.isSneaking) { event.hitEntity.applyDamage(100000000000000000) }
   }
 })
